aee7b5d787d300219a2ebc2587903857445ffa7e
Roberto Carrillo <email@robertocarrillo.com>
2019-08-23 02:19:51 -0500

    Blazor preview 8

--- src/Web1Apps/Web1Apps/Data/WeatherForecast.cs
+++ src/Web1Apps/Web1Apps/Data/WeatherForecast.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System;
 
-namespace Web1Apps.Shared
+namespace Web1Apps.Data
 {
     public class WeatherForecast
     {
@@ -10,8 +8,8 @@ namespace Web1Apps.Shared
 
         public int TemperatureC { get; set; }
 
-        public string Summary { get; set; }
-
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
     }
 }
