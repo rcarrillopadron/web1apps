238c5aec6a4bf8eaae9d9668739df72bc505f004
Emmanuel Servín <e.servin.h@outlook.com>
2019-10-01 12:44:57 -0500

    Task 68

--- src/Web1Apps/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/Web1Apps/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -47,18 +47,18 @@ namespace Web1Apps.Areas.Identity.Pages.Account
         {
             [Required]
             [EmailAddress]
-            [Display(Name = "Email")]
+            [Display(Name = "Correo Electrónico")]
             public string Email { get; set; }
 
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(100, ErrorMessage = "La contraseña debe conetener Mayuscula, número y carácter.", MinimumLength = 6)]
             [DataType(DataType.Password)]
-            [Display(Name = "Password")]
+            [Display(Name = "Contraseña")]
             public string Password { get; set; }
 
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            [Display(Name = "Confirma Contraseña")]
+            [Compare("Password", ErrorMessage = "La contraseñas no coinciden.")]
             public string ConfirmPassword { get; set; }
         }
 
