9ffc1d2232c6a09a54610337345d62cb086432a4
Roberto Carrillo <email@robertocarrillo.com>
2019-12-25 13:52:17 -0600

    Added Api

---
+++ src/Web1Apps/Pages/LocationEditBase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Web1Apps.Services;
+
+namespace Web1Apps.Pages
+{
+    public class LocationEditBase : ComponentBase
+    {
+        [Inject]
+        public ILocationDataService LocationDataService { get; set; }
+
+        [Parameter]
+        public string LocationId { get; set; }
+
+        public Location Location { get; set; } = new Location();
+    }
+
+    
+}
